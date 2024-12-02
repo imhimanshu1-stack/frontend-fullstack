bb36161d5aa6e37cfb6df66727f878120422881e
imhimanshu1-stack <himanshuverma131213@gmail.com>
2024-12-02 16:32:42 +0530

    footer component

---
+++ src/components/footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="app-footer">
+    <p>&copy; 2024 Lesson Booking App. All rights reserved.</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  props: ["Footer"],
+};
+</script>
+<style>
+.app-footer {
+  background-color: #1d2939;
+  text-align: center;
+  color: white;
+  padding: 15px;
+  font-size: 12px;
+}
+</style>
